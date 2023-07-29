baebe853c7c67952e4470b75fff52505aace042e
DESKTOP-GLVMONG\user <wakaba.trial01@gmail.com>
2023-07-29 17:34:49 +0900

    2画面目までのjsの実装

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,33 +1,40 @@
 function a1(a) {
-    var msg = document.getElementById(a);
-    alert(msg.value);
+    $("#"+a).remove();
 }
 
+var idx = 1;
+
 function a2(a){
-    /**var array =[
-        [<ol>
-            <li><input id="1-1" type="text" name="example1"></input><button onclick="a1('1-1')">削除</button></li>
-          </ol>],
-    ]
-    */
     var array =[
-        ""
+        [""],
     ]
     array.forEach(function (value, index) {
         console.log(index + value)
         var text = ""
-        var output = document.getElementById(a)
+        var output = $("#"+a)
         var item = document.createElement("div")
-        output.appendChild(item)
-        value.forEach(function (value2, index2) {
-            text += value2+""
-            item.textContent=text
-        })
-    
+        output.append('<li id="row-'+idx+'"><input id="1-1" class="1-5" type="text" name="example1"></input><button class=1-4 onclick="a1(\'row-'+idx+'\')">削除</button></li>')
+        idx++;
     })
     }
 
 function a3(a) {
-    var msg = document.getElementById(a);
-    alert(msg.value);
+    $(".1-4").each(function(i, elem) {
+        $(elem).replaceWith('<input type="checkbox">');
+    });
+    $(".1-5").each(function(i, elem) {
+        $(elem).prop('disabled', true);
+    });
+    $("#1-6").replaceWith('<h3 style="text-align:right" id="2-1"><button id="2-2" onclick="b1()">完了</button></h3>');
+    $("#1-7").prop('disabled', true);
+}
+
+//location.href="index2.html"　disabled $("#1-4").replaceWith('<input type="checkbox">');
+
+function b1() {
+    location.href="index3.html"
+}
+
+function d1() {
+    location.href="index.html"
 }
